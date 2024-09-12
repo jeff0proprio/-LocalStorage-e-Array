7f6d2d64106fde8211e33ba7c1ab3cfef0796606
Jeff himself <162985697+jeff0proprio@users.noreply.github.com>
2024-09-12 11:48:19 -0300

    Add files via upload

---
+++ Local_Storage/ATV1/file.js
@@ -0,0 +1,5 @@
+function Armazenar(){
+    const Numero = document.getElementById('Numero_Usuario').value
+    document.getElementById('Numero_Usuario').value
+    localStorage.setItem('Numero', Numero)
+}
---
+++ Local_Storage/ATV2/file.js
@@ -0,0 +1,5 @@
+function Armazenar(){
+    const Texto = document.getElementById('Numero_Usuario').value
+    document.getElementById('Numero_Usuario').value
+    localStorage.setItem('mensagem', Texto)
+}
